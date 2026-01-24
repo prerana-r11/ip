877db24e37d6dceca648a6b140e0687bdc8dac9d
prerana-r11 <preranaravi2006@gmail.com>
2026-01-24 23:06:53 +0800

    Add Level 0:rename chatbot and greet user

---
+++ src/main/java/Cosmic.java
@@ -0,0 +1,7 @@
+public class Cosmic {
+    public static void main(String[] args) {
+       System.out.println("Hello! I'm Cosmic");
+       System.out.println("What can I do for you?");
+       System.out.println("Bye. Hope to see you again soon!");
+    }
+}
