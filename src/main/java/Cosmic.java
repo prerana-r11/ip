09a0e4bea3acfcaff3c454e09c840a4016d878cf
prerana-r11 <preranaravi2006@gmail.com>
2026-01-27 23:20:35 +0800

    Add Level 1: echo user input until bye

--- src/main/java/Cosmic.java
+++ src/main/java/Cosmic.java
@@ -1,7 +1,16 @@
+import java.util.Scanner;
 public class Cosmic {
     public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
        System.out.println("Hello! I'm Cosmic");
        System.out.println("What can I do for you?");
-       System.out.println("Bye. Hope to see you again soon!");
+       while(true) {
+           String input = scanner.nextLine();
+           if (input.equals("bye")) {
+               System.out.println("Bye. Hope to see you again soon!");
+               break;
+           }
+           System.out.println(input);
+       }
     }
 }
