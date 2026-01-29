94dfff250cd8fcb712693ee99c99efb363c2699d
prerana-r11 <preranaravi2006@gmail.com>
2026-01-29 20:24:59 +0800

    Add Level-3: mark and unmark tasks using Task class

--- src/main/java/Cosmic.java
+++ src/main/java/Cosmic.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class Cosmic {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String[] tasks = new String[100];
+        Task[] tasks = new Task[100];
         int taskCount = 0;
         System.out.println("Hello! I'm Cosmic");
         System.out.println("What can I do for you?");
@@ -13,14 +13,29 @@ public class Cosmic {
                 break;
             }
            if(input.equals("list")){
+               System.out.println("Here are the tasks in your list:");
                for(int i = 0; i < taskCount; i++){
                    System.out.println((i+1) + ". " + tasks[i]);
                }
                continue;
            }
-           tasks[taskCount] = input;
+           if(input.startsWith("mark")){
+               int index= Integer.parseInt(input.substring(5))-1;
+               tasks[index].markAsDone();
+               System.out.println("Nice! I've marked this task as done:");
+               System.out.println(" " + tasks[index]);
+               continue;
+           }
+           if(input.startsWith("unmark")){
+               int index= Integer.parseInt(input.substring(7))-1;
+               tasks[index].markAsNotDone();
+               System.out.println("OK, I've marked this task as not done yet:");
+               System.out.println(" " + tasks[index]);
+               continue;
+           }
+           tasks[taskCount] = new Task(input);
            taskCount++;
-           System.out.println("added: " +input);
+           System.out.println("Added: " +input);
        }
     }
 }
---
+++ src/main/java/Task.java
@@ -0,0 +1,42 @@
+public class Task {
+    private final String description;
+    private boolean isDone;
+    /**
+     * Creates a task with the given description.
+     *
+     * @param description Description of the task.
+     */
+    public Task(String description) {
+        this.description= description;
+        this.isDone=false;
+    }
+    /**
+     * Marks this task as done.
+     */
+    public void markAsDone() {
+        isDone=true;
+    }
+    /**
+     * Marks this task as not done.
+     */
+    public void markAsNotDone() {
+        isDone=false;
+    }
+    /**
+     * Returns the status icon of the task.
+     *
+     * @return "X" if done, otherwise a space.
+     */
+    public String getStatusIcon() {
+        return isDone ? "X" : " ";
+    }
+    /**
+     * Returns the string representation of this task.
+     *
+     * @return Task formatted as a string.
+     */
+    @Override
+    public String toString() {
+        return "[" +getStatusIcon() +"]" +description;
+    }
+}
