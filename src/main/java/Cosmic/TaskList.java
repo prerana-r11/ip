352338232e4323b03259ab0a1619b35602987cb6
prerana-r11 <preranaravi2006@gmail.com>
2026-02-27 23:15:25 +0800

    Add A-JavaDoc:add javadoc comments

--- src/main/java/Cosmic/TaskList.java
+++ src/main/java/Cosmic/TaskList.java
@@ -1,23 +1,47 @@
 package Cosmic;
 import java.util.ArrayList;
-
+/**
+ * Represents a list of tasks.
+ * Provides operations to add, delete, modify, retrieve,
+ * and search tasks stored in the list.
+ */
 public class TaskList {
     private final ArrayList<Task> tasks;
 
+    /**
+     * Constructs an empty TaskList.
+     */
     public TaskList(){
 
         this.tasks= new ArrayList<>();
     }
 
+    /**
+     * Constructs a TaskList using an existing list of tasks.
+     *
+     * @param tasks The initial list of tasks to store.
+     */
     public TaskList(ArrayList<Task> tasks) {
         this.tasks = tasks;
     }
 
+    /**
+     * Adds a task to the task list.
+     *
+     * @param task The task to be added.
+     */
     public void add(Task task){
 
         tasks.add(task);
     }
 
+    /**
+     * Deletes the task at the specified index.
+     *
+     * @param index The index of the task to delete.
+     * @return The deleted task.
+     * @throws CosmicException If the index is invalid.
+     */
     public Task delete(int index) throws CosmicException{
         if(index<0 || index>tasks.size()){
             throw new CosmicException("Invalid task number!");
@@ -25,6 +49,12 @@ public class TaskList {
         return tasks.remove(index);
     }
 
+    /**
+     * Marks the task at the specified index as completed.
+     *
+     * @param index The index of the task to mark.
+     * @throws CosmicException If the index is invalid.
+     */
     public void mark(int index) throws CosmicException{
         if(index<0 || index>tasks.size()){
             throw new CosmicException("Invalid task number!");
@@ -32,26 +62,54 @@ public class TaskList {
         tasks.get(index).markAsDone();
     }
 
+    /**
+     * Marks the task at the specified index as not completed.
+     *
+     * @param index The index of the task to unmark.
+     * @throws CosmicException If the index is invalid.
+     */
     public void unmark(int index) throws CosmicException{
-        if(index<0 || index>tasks.size()){
+        if(index<0 || index>=tasks.size()){
             throw new CosmicException("Invalid task number!");
         }
         tasks.get(index).markAsNotDone();
     }
 
+    /**
+     * Returns the total number of tasks in the list.
+     *
+     * @return The number of tasks.
+     */
     public int size(){
         return tasks.size();
     }
 
+    /**
+     * Retrieves the task at the specified index.
+     *
+     * @param index The index of the task.
+     * @return The task at the given index.
+     */
     public Task get(int index){
 
         return tasks.get(index);
     }
 
+    /**
+     * Returns the full list of tasks.
+     *
+     * @return The list containing all tasks.
+     */
     public ArrayList<Task> getAll(){
         return tasks;
     }
 
+    /**
+     * Returns a new TaskList containing tasks whose descriptions
+     * contain the specified keyword.
+     *
+     * @return A TaskList containing matching tasks.
+     */
     public TaskList find(String find){
         ArrayList<Task> filteredList = new ArrayList<>();
         for(Task t:tasks){
--- src/main/java/Cosmic/UI.java
+++ src/main/java/Cosmic/UI.java
@@ -1,5 +1,9 @@
 package Cosmic;
 
+/**
+ * Handles all user interface interactions.
+ * Responsible for printing messages and responses to the user.
+ */
 public class UI {
 
     public void printGreeting(){
@@ -7,18 +11,36 @@ public class UI {
         System.out.println("What can I do for you?");
     }
 
+    /**
+     * Prints confirmation after a task is added.
+     *
+     * @param task The task that was added.
+     * @param total The updated total number of tasks.
+     */
     public void printAddedTask(Task task,int total){
         System.out.println("Got it. I've added this task:");
         System.out.println(" " + task);
         System.out.println("Now you have " + total + " tasks in the list.");
     }
 
+    /**
+     * Prints confirmation after a task is deleted.
+     *
+     * @param task The task that was deleted.
+     * @param total The updated total number of tasks.
+     */
     public void printDeletedTask(Task task, int total){
         System.out.println("Noted, I've removed this task");
         System.out.println(" " + task);
         System.out.println(("Now you have " + total + " tasks in your list"));
     }
 
+
+    /**
+     * Prints all tasks in the given TaskList.
+     *
+     * @param tasks The TaskList to be displayed.
+     */
     public void printList(TaskList tasks){
         System.out.println("Here are the tasks in your list:");
         for(int i=0;i< tasks.size();i++){
@@ -26,22 +48,40 @@ public class UI {
         }
     }
 
+    /**
+     * Prints confirmation when a task is marked as  completed.
+     *
+     * @param task The task that was marked.
+     */
     public void printMarked(Task task) {
         System.out.println("Nice! I've marked this task as done:");
         System.out.println(" " + task);
     }
 
+    /**
+     * Prints confirmation when a task is marked as not completed.
+     *
+     * @param task The task that was unmarked.
+     */
     public void printUnmarked(Task task) {
         System.out.println("OK, I've marked this task as not done yet:");
         System.out.println(" " + task);
     }
 
 
+    /**
+     * Prints an error message to the user.
+     *
+     * @param message The error message to display.
+     */
     public void printError(String message) {
 
         System.out.println("OOPS!!! " + message);
     }
 
+    /**
+     * Prints the exit message when the application terminates.
+     */
     public void printExit(){
 
         System.out.println("Bye. Hope to see you again soon!");
